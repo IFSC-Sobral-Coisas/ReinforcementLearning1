ad8c44ca8b439b1ba54edc5e38e904ce4e0fd858
M. M. Sobral <msobral@ifsc.edu.br>
2020-08-25 15:18:40 -0300

    comentários no código

--- dp.py
+++ dp.py
@@ -4,12 +4,15 @@ import random
 
 @attr.s
 class Action:
+    '''Uma ação: especifica uma quantidade de carros movidos, possui um valor q, e a quantidade
+    de carros nos lugares (dstate) se aplicada'''
     n = attr.ib(default=0)
     q = attr.ib(default=0)
     dstate = attr.ib(default=None)
 
 @attr.s
 class State:
+    'Um estado: tem um valor, um conjunto de ações, e uma ação dada pela policy'
     v = attr.ib(default=0)
     actions = attr.ib(factory=list)
     pi = attr.ib(factory=Action)
@@ -18,10 +21,12 @@ class State:
         self.actions.append(a)
 
     def initialize(self):
+        'escolhe uma ação aleatoriamente'
         self.pi = random.choice(self.actions)
 
 @attr.s
 class Place:
+    'Um lugar onde tem uma loja com carros para alugar'
     mreq = attr.ib(default=1)
     mret = attr.ib(default=1)
     states = attr.ib(factory=dict)
@@ -31,9 +36,11 @@ class Place:
         return math.exp(-m)*m**n/math.factorial(n)
 
     def preq(self, n):
+        'Probabilidade de n carros alugados'
         return self._poisson(self.mreq, n)
 
     def pret(self, n):
+        'Probabilidade de n carros devolvidos'
         return self._poisson(self.mret, n)
 
     def prob(self, n):
@@ -57,6 +64,13 @@ class Solver:
     Theta = 0.1
 
     def __init__(self, **args):
+        '''Parâmetors:
+        N:int = quantidade de carros total
+        p1:Place = lugar 1 com suas probabilidades de carros alugados e devolvidos
+        p2:Place = lugar 2 com suas probabilidades de carros alugados e devolvidos
+        r:float = recompensa para cada carro alugado
+        cost:float = custo para mover carro
+        gamma:float = fator de decaimento da recompensa'''
         self.N = args.get('N', Solver.N)
         self.p1 = args.get('p1', Solver.p1)
         self.p2 = args.get('p2', Solver.p2)
@@ -92,50 +106,39 @@ class Solver:
                 a = Action(k, 0, (min(self.N,n[0]+k), n[1]-k))
                 self.states[n].add_action(a)
 
-        # for n in range(self.N+1):
-        #     self.states[(n,self.N-n)] = State()
-        # self.states[n].add_action(Action(0, 1, n))
-        # # Ações que levam carros do lugar 1 para o 2
-        # for k in range(min(n1, 5)):
-        #     k = 1 + k
-        #     a = Action(k, 0, (n1 - k, n2 + k))
-        #     self.states[n].add_action(a)
-        # # Cria as ações possíveis em cada estado
-        # for n,s in self.states.items():
-        #     n1,n2 = n
-        #     # Ação que não move carros inicialmente com prob. 1
-        #     self.states[n].add_action(Action(0,1,n))
-        #     # Ações que levam carros do lugar 1 para o 2
-        #     for k in range(min(n1,5)):
-        #         k = 1+k
-        #         a = Action(k, 0, (n1-k, n2+k))
-        #         self.states[n].add_action(a)
-        #     # Ações que levam carros do lugar 2 para o 1
-        #     for k in range(min(n2,5)):
-        #         k = 1+k
-        #         a = Action(k, 0, (n1+k, n2-k))
-        #         self.states[n].add_action(a)
-
     def requested(self, s, a:Action):
         r = -a.n*self.cost
 
     def evaluate(self, s:State, a:Action):
-        n1 = a.dstate
-        v = 0
+        'Calcula o valor do estado s se for seguida a ação a'
+        n1 = a.dstate # estado destino da ação
+        v = 0 # valor a calcular
+        # Para cada estado (n2: tupla (np1,np2), s2: estado)
         for n2, s2 in self.states.items():
-            dn1 = n2[0] - n1[0]
-            dn2 = n2[1] - n1[1]
+            dn1 = n2[0] - n1[0] # diferença de carros no lugar 1, entre estado atual n1 e destino n2
+            dn2 = n2[1] - n1[1] # diferença de carros no lugar 2, entre estado atual n1 e destino n2
             ck1 = set()
+            # rq1: carros alugados, e rt1: carros retornados
             for rq1 in range(n1[0]):
+                # carros retornados: calculado pela variação de carros observada,
+                # e a quantidade de carros alugados. Ex:
+                # a) se a variação foi 1, e carros alugados foi 1, então retornados
+                # deve ser 2.
+                # b) Se variação foi -1 e carros alugados foi 1, retornados deve ser 0.
                 rt1 = max(0,dn1 + rq1)
-                if rt1 in ck1: continue
+                if rt1 in ck1: continue # se já calculou este valor, ignora
                 ck1.add(rt1)
                 ck2 = set()
+                # faz o mesmo procedimento para a variação de carros no lugar 2
                 for rq2 in range(n1[1]):
                     rt2 = max(0, dn2 + rq2)
                     if rt2 in ck2: continue
                     ck2.add(rt2)
+                    # recompensa: quanto se ganha com esse balanço de carros movidos e alugados
                     r = -a.n * self.cost + (rq1 + rq2) * self.r + self.gamma * v
+                    # valor: pondera a recompensa pela probabilidade de que ocorra
+                    # produto das probabilidades de ocorrerem estas quantidades de carros alugados
+                    # e devolvidos nos lugares 1 e 2
                     v += self.p1.preq(rq1) * self.p1.pret(rt1) * self.p2.preq(rq2) * self.p2.pret(rt2) * r
         return v
 
@@ -147,17 +150,23 @@ class Solver:
             delta = self.Theta+1
             while delta >= self.Theta:
                 delta = 0
+                # para cada estado (n: tupla (n1, n2), s: estado)
                 for n,s in self.states.items():
-                    v = s.v
+                    v = s.v # valor atual do estado
                     s.v = 0
-                    a = s.pi
-                    s.v = self.evaluate(s, a)
-                    delta = max(delta, abs(v-s.v))
+                    a = s.pi # a ação escolhida pela policy
+                    s.v = self.evaluate(s, a) # calcula o valor do estado com esta ação
+                    delta = max(delta, abs(v-s.v)) # atualiza o delta, que é a variação de ganho no valor
             # 3. policy improvement
             stable = True
+            # para cada estado (n: tupla (n1, n2), s: estado)
             for n,s in self.states.items():
-                old_a = s.pi
+                old_a = s.pi # ação da policy atual para estado "s"
+                # avalia cada uma das ações do estado "s", e seleciona
+                # aquela que retornou o maior valor. Ela será a nova policy
                 s.pi = max(s.actions, key=lambda a: self.evaluate(s, a))
+                # Se ação for diferente da atual, mas delta > 0 (no passo 2 houve atualização de valores)
+                # então ainda é possível melhorar ...
                 if old_a != s.pi and delta > 0: stable = False
 
 
